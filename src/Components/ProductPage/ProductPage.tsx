65b8d9c1d11733c9a0d02cbf96e61638c1a13c83
Byanew23 <borislav.yanev23@gmail.com>
2024-01-14 15:45:07 +0200

    add gifing images

--- src/Components/ProductPage/ProductPage.tsx
+++ src/Components/ProductPage/ProductPage.tsx
@@ -11,14 +11,37 @@ export const ProductPage = () => {
     const [item, setItem] = React.useState<glassesDataType | undefined>(undefined)
     window.scrollTo(0, 0)
     const [openModal, setOpenModal] = React.useState<boolean>(false)
+    const [currentPic, setCurrentPic] = React.useState<number>(0)
+    const [direction, setDirection] = React.useState<boolean>(true);
 
     // Fetch product details based on productId or use static data
     if (!item) getItemById(productId || '').then(data => setItem(data))
 
-    console.log(openModal)
+    React.useEffect(() => {
+        if (item?.urls.length && item.urls.length > 1) {
+            const intervalId = setInterval(() => {
+
+                // Update the current index based on the direction
+                setCurrentPic((prevIndex) => {
+                    const newIndex = prevIndex + (direction ? 1 : -1);
+
+                    // Change direction when reaching the ends of the array
+                    if (newIndex >= item.urls.length - 1 || newIndex <= 0) {
+                        setDirection(!direction);
+                    }
+
+                    return newIndex;
+                });
+            }, 100);
+            // Cleanup function to clear the interval when the component is unmounted
+
+            return () => clearInterval(intervalId);
+        }
+    }, [currentPic, direction, item])
+
 
     return item ? <div className="product-wrapper">
-        <img className="product-image" src={item.urls[0]} alt={item.name} />
+        <img className="product-image" src={item.urls[currentPic]} alt={item.name} />
         <span>
 
             <div className="details">
