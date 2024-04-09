6d5765e7adcca9ca78d0152423fa44f32ff07431
Byanew23 <borislav.yanev23@gmail.com>
2024-04-09 12:31:11 +0300

    test spline

---
+++ src/Components/RenderMWR/RenderMWR.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './styleMWR.css'
+import { Application } from '@splinetool/runtime'
+
+const splineLink = 'https://prod.spline.design/5M1yyxJPpuyS3au9/scene.splinecode'
+export const RenderMWR = () => {
+    // const canvas = document.getElementById('canvas3d')
+    React.useEffect(() => {
+        const canvas = document.getElementById('canvas3d')
+
+        console.log(canvas)
+
+        const spline = new Application(canvas as any)
+        spline.load(splineLink)
+    }, [])
+
+    return <div>
+        <canvas id='canvas3d' />
+    </div>
+}
